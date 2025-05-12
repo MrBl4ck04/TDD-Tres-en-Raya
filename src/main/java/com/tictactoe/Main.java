288db39237172091319c33a118a8a6a5429b45eb
inaDAGod <danialee14@gmail.com>
2025-05-12 16:38:22 -0400

    update main

--- src/main/java/com/tictactoe/Main.java
+++ src/main/java/com/tictactoe/Main.java
@@ -12,6 +12,6 @@ public class Main {
      */
     public static void main(String[] args) {
         System.out.println("¡Bienvenido al juego de Tres en Raya!!!! Prueba\n");
-        System.out.println("Próximamente estará disponible la versión completa.");
+        System.out.println("Próximamente estará disponible la versión completa. <3");
     }
 }
