c8aa7f405e544999f924d6fba0aff984c38c64ce
inaDAGod <danialee14@gmail.com>
2025-05-18 18:21:25 -0400

    REFACTOR :R2 (function: despuesDeXJuegaO)

--- src/main/java/com/tictactoe/controller/Game.java
+++ src/main/java/com/tictactoe/controller/Game.java
@@ -31,7 +31,9 @@ public class Game {
     /** Aplica un turno completo: move + cambio de turno */
     public void step() {
         playTurn();
-        current = p1;
+        if (!isOver()) {
+            current = (current == p1 ? p2 : p1);
+        }
     }
 
     /** ¿Se terminó el juego? */
