20c06dc30e3af5d1df9bb443c15bb9d4618ce3e7
Jhuly1215 <jhuliannnavitoria@gmail.com>
2025-05-18 15:47:21 -0400

    RED: make fail R3 (function: HayVictoriaDiagonal)

--- src/test/java/com/tictactoe/model/VictoryTest.java
+++ src/test/java/com/tictactoe/model/VictoryTest.java
@@ -28,7 +28,7 @@ class VictoryTest {
         assertTrue(tablero.hayVictoria('X'), "X debe ganar con fila 1 completa");
         assertFalse(tablero.hayVictoria('O'), "O no debe ganar");
     }
-    /** GREEN REQUERIMIENTO 3: línea vertical completa => victoria */
+    /** REQUERIMIENTO 3: línea vertical completa => victoria */
     @Test
     void victoriaVertical() {
         Board tablero = new Board();
@@ -37,6 +37,23 @@ class VictoryTest {
         tablero.colocarFicha(2, 2, 'O');
         assertTrue(tablero.hayVictoria('O'), "O debe ganar con columna 2 completa");
         assertFalse(tablero.hayVictoria('X'), "X no debe ganar");
+    }
+    /** RED REQUERIMIENTO 4: línea diagonal completa => victoria */
+    @Test
+    void victoriaDiagonal() {
+        Board tablero = new Board();
+        // diagonal principal
+        tablero.colocarFicha(0, 0, 'X');
+        tablero.colocarFicha(1, 1, 'X');
+        tablero.colocarFicha(2, 2, 'X');
+        assertTrue(tablero.hayVictoria('X'), "X debe ganar en diagonal principal");
+
+        // diagonal secundaria
+        Board t2 = new Board();
+        t2.colocarFicha(0, 2, 'O');
+        t2.colocarFicha(1, 1, 'O');
+        t2.colocarFicha(2, 0, 'O');
+        assertTrue(t2.hayVictoria('O'), "O debe ganar en diagonal secundaria");
     }
     /** REQUERIMIENTO 1: test de empate usando un nuevo método hayEmpate() que aún no existe */
     @Test
